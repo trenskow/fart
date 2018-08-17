7b6c7f831bc95e3360565a0222444c8f82234f36
Kristian Trenskow <trenskow@me.com>
2018-08-17 08:46:32 +0200

    Initial Commit

---
+++ fart/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fart
+//
+//  Created by Kristian Trenskow on 17/08/2018.
+//  Copyright © 2018 Kristian Trenskow. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
