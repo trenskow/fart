8bc6b998c6d41ee130c1366af251c26ab60cb111
Kristian Trenskow <trenskow@me.com>
2021-01-17 10:46:58 +0100

    Use typedefs.

--- types/number.hpp
+++ types/number.hpp
@@ -21,9 +21,12 @@ namespace fart::types {
 		integer
 	};
 
-	class Boolean;
-	class Float;
-	class Integer;
+	template<typename T>
+	class Number;
+
+	typedef Number<bool> Boolean;
+	typedef Number<double> Float;
+	typedef Number<int64_t> Integer;
 
 	template<typename T>
 	class Number : public Type {
@@ -117,29 +120,6 @@ namespace fart::types {
 		operator T() {
 			return value();
 		}
-
-	};
-
-	class Boolean: public Number<bool> {
-
-	public:
-		Boolean(const bool value) : Number(value, Subtype::boolean) {}
-		virtual ~Boolean() {}
-	};
-
-	class Float: public Number<double> {
-
-	public:
-		Float(const double value) : Number(value, Subtype::floatingPoint) {}
-		virtual ~Float() {}
-
-	};
-
-	class Integer: public Number<int64_t> {
-
-	public:
-		Integer(const int64_t value) : Number(value, Subtype::integer) {}
-		virtual ~Integer() {}
 
 	};
 
